a8f9200386a30769270829c156f5d1b3a15763f0
Josh Wainwright <josh.wainwright@ldra.com>
2014-11-06 16:20:34 +0000

    Change comment chars to be more readable

--- startup.bat
+++ startup.bat
@@ -1,19 +1,23 @@
-rem Map certain folders to drive letters.
-rem ---------------------------------------------------------------------------
+:: Map certain folders to drive letters.
+:: ---------------------------------------------------------------------------
 subst r: c:\_in_out
 subst h: c:\home\JoshWainwright
 
 
-rem Remove everythin inside the "tmp" directory
-rem ---------------------------------------------------------------------------
-set targetdir="c:\tmp"
-del /q %targetdir%\*
-for /d %%x in (%targetdir%\*) do @rd /s /q ^"%%x^"
+:: Remove everythin inside the "tmp" directory
+:: ---------------------------------------------------------------------------
+set tmpdir="c:\tmp"
+del /q %tmpdir%\*
+for /d %%x in (%tmpdir%\*) do @rd /s /q ^"%%x^"
 
 
-rem Set flags in Testbed.ini
-rem ---------------------------------------------------------------------------
+:: Set flags in Testbed.ini
+:: ---------------------------------------------------------------------------
 set TBINI="C:\LDRA_Toolsuite\946\TBini.exe"
 
 %TBINI% -Section="C/C++ LDRA Testbed" "SHOW_CMTOOL_MENU=TRUE"
 %TBINI% -Section="C/C++ LDRA Testbed" "CM_TOOL_SELECTED=Git"
+
+:: Start Skype otherwise it won't start as the tmp files have been deleted
+:: ---------------------------------------------------------------------------
+"C:\Program Files (x86)\Skype\Phone\Skype.exe"
