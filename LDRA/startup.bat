c63a0ca8a943df891a4bde5cd7132cae011d1b80
Josh Wainwright <josh.wainwright@ldra.com>
2014-11-28 15:43:17 +0000

    Move startup file to correct folder

---
+++ LDRA/startup.bat
@@ -0,0 +1,36 @@
+::
+:: Map certain folders to drive letters.
+::
+subst r: c:\_in_out
+subst h: c:\home\JoshWainwright
+subst l: c:\home\JoshWainwright\TLPs
+
+::
+:: Remove everythin inside the "tmp" directory
+::
+set tmpdir="c:\tmp"
+del /q %tmpdir%\*
+for /d %%x in (%tmpdir%\*) do @rd /s /q ^"%%x^"
+mkdir %tmpdir%\jaw
+
+::
+:: Set flags in Testbed.ini
+::
+set TBINI="C:\LDRA_Toolsuite\C_C++\946\TBini.exe"
+for %%x in (
+	"C/C++ LDRA Testbed"
+	"Ada95 LDRA Testbed"
+	) do (
+	%TBINI% -Section=%%x SHOW_CMTOOL_MENU=TRUE
+	%TBINI% -Section=%%x CM_TOOL_SELECTED=Git
+
+	%TBINI% -Section=%%x REDIRECT_MAILTO=TRUE
+
+	%TBINI% -Section=%%x TBBROWSE_HTML_FONT_SIZE=2
+)
+
+::
+:: Start Skype otherwise it won't start as the tmp files have been deleted
+::
+taskkill /IM skype.exe /F
+start "Skype" /B /MIN /LOW "C:\Program Files (x86)\Skype\Phone\Skype.exe"
