4ee29dbf29cc6f49477df94c7fa84373fb4e2362
Josh Wainwright <josh.wainwright@ldra.com>
2015-04-15 13:23:22 +0100

    update to include 950

--- LDRA/settings.sh
+++ LDRA/settings.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Created:  Mon 02 Feb 2015
-# Modified: Thu 05 Mar 2015
+# Modified: Tue 14 Apr 2015
 # Author:   Josh Wainwright
 # Filename: settings.sh
 
 # Languages
-lang_c=(946)
-lang_a=(946 947)
-lang_j=(946)
+lang_c=(946     950)
+lang_a=(946 947    )
+lang_j=(946        )
 
 declare -A langs=(["lang_c"]="C_C++" ["lang_a"]="Ada" ["lang_j"]="Java")
 
