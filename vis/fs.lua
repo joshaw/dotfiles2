b008a88aa6c9bbc16d2f17de55a9f2456d97e70d
Josh Wainwright <wainwright.ja@gmail.com>
2016-05-27 08:20:09 +0100

    [vis] maxline, allow bang for including tabs in count

--- vis/fs.lua
+++ vis/fs.lua
@@ -1,5 +1,5 @@
 -- Created:  Fri 20 May 2016
--- Modified: Fri 20 May 2016
+-- Modified: Thu 26 May 2016
 -- Author:   Josh Wainwright
 -- Filename: fs.lua
 
@@ -14,6 +14,10 @@ fs.remove = function(fname)
 	end
 end
 
+vis:command_register('RemoveFile', function(argv, force, win, cursor, range)
+	fs.remove(argv[1] or win.file.name)
+end)
+
 fs.rename = function(oldname, newname)
 	local ok, err, _ = os.rename(oldname, newname)
 	if ok then
@@ -22,25 +26,6 @@ fs.rename = function(oldname, newname)
 		vis:info('Could not rename file. ' .. err)
 	end
 end
-
-fs.maxline = function(win)
-	local line
-	local max = 0
-	local cnt = 1
-	for line in win.file:lines_iterator() do
-		if #line > max then
-			max = #line
-			maxl = cnt
-		end
-		cnt = cnt + 1
-	end
-	vis:info('Max line is line ' .. maxl .. ' (' .. max .. ' chars)')
-	win.cursor:to(maxl, 1)
-end
-
-vis:command_register('RemoveFile', function(argv, force, win, cursor, range)
-	fs.remove(argv[1] or win.file.name)
-end)
 
 vis:command_register('RenameFile', function(argv, force, win, cursor, range)
 	local oldname, newname
@@ -56,8 +41,27 @@ vis:command_register('RenameFile', function(argv, force, win, cursor, range)
 	fs.rename(oldname, newname)
 end)
 
+fs.maxline = function(win, force)
+	local line
+	local max = 0
+	local cnt = 1
+	for line in win.file:lines_iterator() do
+		if force then
+			vis:info('Force')
+			line = line:gsub('\t', '    ')
+		end
+		if #line > max then
+			max = #line
+			maxl = cnt
+		end
+		cnt = cnt + 1
+	end
+	vis:info('Max line is line ' .. maxl .. ' (' .. max .. ' chars)')
+	win.cursor:to(maxl, 1)
+end
+
 vis:command_register('MaxLine', function(argv, force, win, cursor, range)
-	fs.maxline(win)
+	fs.maxline(win, force)
 end)
 
 return fs
