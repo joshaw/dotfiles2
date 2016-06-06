b2cedefd55eee614e089afef6af57f01950cdce6
Josh Wainwright <josh.wainwright@ldra.com>
2016-06-06 14:53:36 +0100

    [vis] remove unused user commands

--- vis/fs.lua
+++ vis/fs.lua
@@ -1,45 +1,9 @@
 -- Created:  Fri 20 May 2016
--- Modified: Thu 26 May 2016
+-- Modified: Tue 31 May 2016
 -- Author:   Josh Wainwright
 -- Filename: fs.lua
 
 local fs = {}
-
-fs.remove = function(fname)
-	local ok, err, _ = os.remove(fname)
-	if ok then
-		vis:info('Removed ' .. fname)
-	else
-		vis:info('Could not remove file. ' .. err)
-	end
-end
-
-vis:command_register('RemoveFile', function(argv, force, win, cursor, range)
-	fs.remove(argv[1] or win.file.name)
-end)
-
-fs.rename = function(oldname, newname)
-	local ok, err, _ = os.rename(oldname, newname)
-	if ok then
-		vis:info('Renamed ' .. oldname .. ' to ' .. newname)
-	else
-		vis:info('Could not rename file. ' .. err)
-	end
-end
-
-vis:command_register('RenameFile', function(argv, force, win, cursor, range)
-	local oldname, newname
-	if argv[2] then
-		oldname = argv[1]
-		newname = argv[2]
-	elseif argv[1] then
-		oldname = win.file.name
-		newname = argv[1]
-	else
-		vis:info('Not enough arguments')
-	end
-	fs.rename(oldname, newname)
-end)
 
 fs.maxline = function(win, force)
 	local line
